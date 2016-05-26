3366d0048485bc0ae6c57e3dac9cdc938fd9cc37
omakhobei <tajcig@ya.ru>
2016-05-26 17:54:55 +0300

    init

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,21 @@
+package sample;
+
+import static spark.Spark.*;
+
+/**
+ * Created by oleh on 26.05.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello oleh");
+
+        get("/hello" ,(request, response) -> "Hello from get");
+
+        post("/hello",(req,resp)-> "Hello from post");
+
+        put("/hello",((request, response) -> "Hello from put"));
+
+        delete("/hello",(req,resp)-> "Hello from delete");
+    }
+}
